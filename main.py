74cb6484c46831116340fcb7bc4ddb00972dabf9
the0807 <the0807.eom@gmail.com>
2025-01-13 07:24:03 +0900

    add requirements

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import schedule
 from tool.report import daily_report, check_server_status
 
 # 스케줄 설정
-schedule.every().day.at("07:15").do(daily_report)  # 하루 1번 보고
-#schedule.every(1).minutes.do(check_server_status)   # 매 1분마다 상태 확인
+schedule.every().day.at("08:00").do(daily_report)  # 하루 1번 보고
+schedule.every(1).minutes.do(check_server_status)   # 매 1분마다 상태 확인
 
 # 메인 루프
 if __name__ == "__main__":
